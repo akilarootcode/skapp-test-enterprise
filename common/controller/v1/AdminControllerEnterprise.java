5945b1794cc93481d3b3f829643ba4785fe170b5
Akila Sachintha <akila.silva@rootcode.io>
2024-09-10 10:11:25 +0530

    added sub modules

---
+++ common/controller/v1/AdminControllerEnterprise.java
@@ -0,0 +1,9 @@
+package com.skapp.enterprise.common.controller.v1;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AdminControllerEnterprise {
+}
